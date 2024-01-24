cdaf38703e0398f422affc6f225d35857c5b29b5
root <root@linux.myguest.virtualbox.org>
2024-01-23 23:02:59 -0300

    infa codigo

---
+++ infa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 unzip -y
+cd /tmp
+apt install wget -y
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
